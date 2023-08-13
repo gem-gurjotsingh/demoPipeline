b1e8d5a8843b1ef15dc6ad3c6c1a5f6808ffad34
Gurjot Singh <Gurjot.Singh@Geminisolutions.com>
2023-08-14 00:25:54 +0530

    version change

--- src/main/java/com/example/demoPipeline/controller/UserController.java
+++ src/main/java/com/example/demoPipeline/controller/UserController.java
@@ -4,7 +4,6 @@ import com.example.demoPipeline.entity.User;
 import com.example.demoPipeline.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
